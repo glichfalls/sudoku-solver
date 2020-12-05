8ab195cf3b06245843598a69a692bd5e38a29888
Robin Portner <robin.portner@hotmail.com>
2020-12-05 10:19:31 +0100

    added tests

--- src/test/java/ch/juventus/solver/SudokuSolverTest.java
+++ src/test/java/ch/juventus/solver/SudokuSolverTest.java
@@ -1,21 +1,26 @@
 package ch.juventus.solver;
 
+import ch.juventus.exceptions.ImportException;
 import ch.juventus.exceptions.InvalidFieldException;
 import ch.juventus.importer.SudokuImporter;
 import org.junit.Test;
-
-import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class SudokuSolverTest {
 
     @Test
-    public void testValidSolve() throws IOException, InvalidFieldException {
+    public void testValidSolve() throws InvalidFieldException, ImportException {
         SudokuImporter importer = new SudokuImporter();
         SudokuSolver solver = new SudokuSolver();
-        boolean solved = solver.solve(importer.read(getClass().getResource("/test.txt").getPath()));
-        assertTrue(solved);
+        assertTrue(solver.solve(importer.read(getClass().getResource("/test.txt").getPath())));
+    }
+
+    @Test
+    public void testUnsolvable() throws InvalidFieldException, ImportException {
+        SudokuImporter importer = new SudokuImporter();
+        SudokuSolver solver = new SudokuSolver();
+        assertFalse(solver.solve(importer.read(getClass().getResource("/unsolvable.txt").getPath())));
     }
 
 }
