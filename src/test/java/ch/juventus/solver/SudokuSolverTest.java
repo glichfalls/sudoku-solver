d546d956a9bc69e70d8396611813817ea270ec87
Robin Portner <robin.portner@hotmail.com>
2020-12-05 15:17:18 +0100

    changed solver test, fixed solution error

--- src/test/java/ch/juventus/solver/SudokuSolverTest.java
+++ src/test/java/ch/juventus/solver/SudokuSolverTest.java
@@ -1,27 +1,29 @@
 package ch.juventus.solver;
 
 import ch.juventus.exceptions.ImportException;
+import ch.juventus.exceptions.InvalidFieldException;
 import ch.juventus.exceptions.UnsolvableException;
+import ch.juventus.importer.PuzzleImporter;
 import ch.juventus.importer.SudokuImporter;
 import ch.juventus.puzzle.Sudoku;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class SudokuSolverTest {
 
     @Test
-    public void testValidSolve() throws UnsolvableException, ImportException {
+    public void testValidSolve() throws UnsolvableException, ImportException, InvalidFieldException, IOException {
 
-        SudokuImporter importer = new SudokuImporter();
-        SudokuSolver solver = new SudokuSolver();
+        PuzzleImporter<Sudoku> importer = new SudokuImporter();
+        SolverInterface<Sudoku> solver = new SudokuSolver();
 
         Sudoku solution = importer.read(getClass().getResource("/solution.txt").getPath());
         Sudoku test = importer.read(getClass().getResource("/test.txt").getPath());
 
         solver.solve(test);
-
-        test.print();
 
         assertArrayEquals(solution.get(), test.get());
 
