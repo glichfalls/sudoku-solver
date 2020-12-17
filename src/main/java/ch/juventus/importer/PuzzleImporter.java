7ad85a699386945c6e073dfbd0ab84f3ac7c06e5
Robin Portner <r.portner@inetworx.ch>
2020-12-17 08:46:35 +0100

    added comments to interfaces

--- src/main/java/ch/juventus/importer/PuzzleImporter.java
+++ src/main/java/ch/juventus/importer/PuzzleImporter.java
@@ -5,8 +5,20 @@ import ch.juventus.puzzle.Puzzle;
 
 public interface PuzzleImporter<T extends Puzzle> {
 
+    /**
+     * Load a puzzle from a file
+     * @param path the path to the file
+     * @return the created puzzle
+     * @throws ImportException if the puzzle cannot be created
+     */
     T getPuzzleFromFile(String path) throws ImportException;
 
+    /**
+     * Load a puzzle from a JSON string
+     * @param puzzle decoded json data
+     * @return the created puzzle
+     * @throws ImportException if the puzzle cannot be created
+     */
     T getPuzzleFromString(String puzzle) throws ImportException;
 
 }
--- src/main/java/ch/juventus/loader/PuzzleLoader.java
+++ src/main/java/ch/juventus/loader/PuzzleLoader.java
@@ -2,8 +2,17 @@ package ch.juventus.loader;
 
 public interface PuzzleLoader {
 
+    /**
+     * Get random puzzle data JSON format as string with the selected difficulty
+     * @param level the difficulty
+     * @return decoded JSON string
+     */
     String getRandom(int level);
 
+    /**
+     * Get random puzzle data JSON format as string
+     * @return decoded JSON string
+     */
     String getRandom();
 
 }
